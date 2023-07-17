d6eb3f20ba0608336e7777cc92e0f0c7f1746670
geonheegit <geonhee625@gmail.com>
2023-07-17 12:12:00 +0900

    암호화 복호화 완성
    와 ㅁㅊ 드디어 됐다

--- main.py
+++ main.py
@@ -79,17 +79,12 @@ def adjust_list(lst): # 숫자 30은 빈공간을 의미
 
     return lst
 
-mode = int(input("숫자를 입력하세요.\n①: 암호화, ②: 복호화\n▶ "))
-
-def encrypt(input, password):
-
-if mode == 1:
-    user_input = input("문자열을 입력하세요: ")
-    converted_numbers = convert_string_to_numbers(user_input)
+def encrypt_or_decrypt(input, password, mode):
+    converted_numbers = convert_string_to_numbers(input)
 
     foursliced_numbers = []
     for i in range(0, len(converted_numbers), 2):
-        group = converted_numbers[i:i+2]
+        group = converted_numbers[i:i + 2]
         foursliced_numbers.append(group)
 
     print(foursliced_numbers)
@@ -98,7 +93,7 @@ if mode == 1:
 
     foursliced_numbers_22_adjusted = []
     for i in range(0, len(filled_fs_numbers), 2):
-        group = filled_fs_numbers[i:i+2]
+        group = filled_fs_numbers[i:i + 2]
         foursliced_numbers_22_adjusted.append(group)
     # print(f"빈 공간 채워진 2x2 리스트: {foursliced_numbers_22_adjusted}")
 
@@ -106,19 +101,11 @@ if mode == 1:
     for i in foursliced_numbers_22_adjusted:
         arrayed_list = np.array(i)
         foursliced_numbers_22_adjusted_np.append(arrayed_list)
-    # print(foursliced_numbers_22_adjusted_np)
 
-    # 행렬 추출할 때는 이렇게.
-    # print(foursliced_numbers_22_adjusted_np[0], foursliced_numbers_22_adjusted_np[1])
-
-    # foursliced_numbers_22_adjusted_np = np.array(foursliced_numbers_22_adjusted)
-    # print(f"빈 공간 채워진 2x2 넘파이 행렬: {foursliced_numbers_22_adjusted_np}")
-    # print(foursliced_numbers_22_adjusted_np.shape)
-
-    user_password = input("4자리 숫자 비밀번호를 입력하세요: ")
+    # =============================
     password_numbers = []
-    for i in range(0, len(user_password)):
-        each_num = user_password[i]
+    for i in range(0, len(password)):
+        each_num = password[i]
         password_numbers.append(int(each_num))
 
     # 비밀번호 리스트를 2x2 넘파이 배열로
@@ -129,15 +116,28 @@ if mode == 1:
     # 2x2씩 쪼개인 문자열 행렬과 비밀번호 행렬의 행렬곱 연산
     encrypted_matrices = []
     for i in foursliced_numbers_22_adjusted_np:
-        result_matrix = i @ password_numbers_np # 행렬곱 연산
+        result_matrix = i @ password_numbers_np  # 행렬곱 연산
         encrypted_matrices.append(result_matrix)
+    # ==============================
+    if mode == 1:
+        print(f"입력한 문장: {user_input}")
+        print(f"비밀번호: {user_password}")
+        print(f"암호화된 문장: {forced_decryption(encrypted_matrices)}")
 
-    print(f"입력한 문장: {user_input}")
-    print(f"비밀번호: {user_password}")
-    print(f"암호화된 문장: {forced_decryption(encrypted_matrices)}")
+        np.save("data\encrypted_sentence_mat.npy", encrypted_matrices)
+        # print(f"복호화된 문장: {decrypt(encrypted_matrices, password_numbers_np)}")
+
+    elif mode == 2:
+        encrypted_matrices = np.load("data/encrypted_sentence_mat.npy")
+        print(f"복호화된 문장: {decrypt(encrypted_matrices, password_numbers_np)}")
+
+mode = int(input("숫자를 입력하세요.\n①: 암호화, ②: 복호화\n▶ "))
+
+user_input = input("문자열을 입력하세요: ")
+user_password = input("4자리 숫자 비밀번호를 입력하세요: ")
+
+if mode == 1:
+    encrypt_or_decrypt(user_input, user_password, 1)
 
 elif mode == 2:
-    print(f"복호화된 문장: {decrypt(encrypted_matrices, password_numbers_np)}")
-# print(encrypted_matrices)
-
-
+    encrypt_or_decrypt(user_input, user_password, 2)
